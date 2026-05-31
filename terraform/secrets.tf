af4ca08b71c102c1313581df711e2836f78b8115
raffidevaa <raffidevaa@gmail.com>
2026-06-01 00:36:18 +0700

    refactor: pull env var from secret manager

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -10,3 +10,16 @@ resource "google_secret_manager_secret_iam_member" "access" {
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${var.PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
 }
+
+resource "google_secret_manager_secret" "api_url" {
+  secret_id = "NEXT_PUBLIC_API_URL"
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "api_url_access" {
+  secret_id = google_secret_manager_secret.api_url.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${var.PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
+}
