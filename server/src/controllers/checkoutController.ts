25be19ee6d1b5607aff47dcb50281fb9a991a851
raffidevaa <raffidevaa@gmail.com>
2026-06-01 00:00:43 +0700

    fix: lint issue on checkout controller

--- server/src/controllers/checkoutController.ts
+++ server/src/controllers/checkoutController.ts
@@ -119,7 +119,7 @@ export const capturePaypalTransaction = async (req: Request, res: Response) => {
 
   // 3. Call PayPal to capture the order
   const request = new paypal.orders.OrdersCaptureRequest(orderID);
-  request.requestBody({} as any);
+  request.requestBody({});
 
   try {
     const capture = (await client().execute(request)) as PaypalCaptureResponse;
